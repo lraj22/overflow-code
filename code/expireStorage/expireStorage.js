2572f67a4e8cec44faf182d7482a6a78b3a1b065
Lakshya Raj <63673413+lraj22@users.noreply.github.com>
2022-06-27 14:48:15 -0700

    Create expireStorage.js

---
+++ code/expireStorage/expireStorage.js
@@ -0,0 +1,39 @@
+window.expireStorage={
+	"storage": sessionStorage, // localStorage and localforage are also options if you change this
+	"deleteOnExpiredRead": true, // delete if expired and tried to read?
+	"setItem": function(key, value, expires){
+		expireStorage.storage.setItem(key, JSON.stringify({"e": Date.now()+parseInt(expires), "v": value}));
+	},
+	"getItem": function(name){
+		try{
+			var result = expireStorage.storage.getItem(name);
+			if(!result)
+				return {"msg": "Not set"};
+			result = JSON.parse(result);
+		} catch(s){
+			if(s instanceof SyntaxError)
+				return {"msg": "Not expireStorage key"};
+			else
+				return {"msg": "Error...?"};
+		}
+		if(Date.now() > result.e){
+			if(expireStorage.deleteOnExpiredRead)
+				expireStorage.storage.removeItem(name);
+			return {"msg": "Expired", "expires": result.e};
+		}
+		return {"msg": "Success", "expires": result.e, "val": result.v};
+	},
+	"deleteExpired": function(){
+		var length = expireStorage.storage.length, key, value;
+		if(typeof length === "function")
+			length = length();
+		for(var i = 0; i < length; i++){
+			try{
+				key = expireStorage.storage.key(i);
+				value = JSON.parse(expireStorage.storage.getItem(key));
+				if(Date.now() > result.e)
+					expireStorage.storage.removeItem(key);
+			} catch(s){}
+		}
+	}
+};
